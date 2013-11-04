0c2c7a0b64d989664ea62ef64704112f4f524be1
Dmitry Vershinin <dvershinin@rtcsoftware.ru>
2013-11-04 23:28:22 +0400

    Реализация шаблона ThreadPool.

---
+++ thread_pool.rb
@@ -0,0 +1,37 @@
+require 'thread'
+
+MAX_THREADPOOL_SIZE = 20
+
+class ThreadPool
+	def initialize(size)
+		if size > MAX_THREADPOOL_SIZE
+			size = MAX_THREADPOOL_SIZE
+		end
+		@size = size
+		@jobs = Queue.new
+
+		@pool = Array.new(@size) do |i|
+			Thread.new do
+				Thread.current[:id] = i
+				catch(:exit) do
+					loop do
+						job, args = @jobs.pop
+						job.call(*args)
+					end
+				end
+			end
+		end
+	end
+
+	def schedule(*args, &block)
+		@jobs << [block, args]
+	end
+
+	def shutdown
+		@size.times do
+			schedule { throw :exit }
+		end
+
+		@pool.map(&:join)
+	end
+end
