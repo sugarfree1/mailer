5df23290c05ececafb7b7f59c4e3544e86150afc
Dmitry Vershinin <dvershinin@rtcsoftware.ru>
2013-11-07 01:16:17 +0400

    Рефакториг отправки письма: объединение доступа и метода отправки в один класс.

--- child.rb
+++ child.rb
@@ -6,7 +6,7 @@ require 'pony'
 require 'colorize'
 require './thread_pool'
 require './settings'
-require './mail'
+require './mailer'
 
 def run(pool_size, port="8000")
 	$tp = ThreadPool.new(pool_size)
@@ -16,37 +16,10 @@ end
 post '/mail' do
 	@json = JSON.parse(request.body.read)
 	$tp.schedule do
-		send_mail(EMAIL_CREDENTIALS, @json)
+		mailer = Mailer.new(EMAIL_CREDENTIALS)
+		mailer.send(@json)
 	end
 end
 
-def send_mail(credentials, data)
 
-	begin
-		mail_credentials = MailCredentials.new(credentials)
-		Pony.mail(:to => @json.has_key?("to") ? @json["to"] : raise('No mail recipient.'), 
-			:from => mail_credentials.from,
-			:via => :smtp, 
-			:via_options => {
-				:address => mail_credentials.address,
-				:port => mail_credentials.port,
-				:user_name => mail_credentials.user_name,
-				:password => mail_credentials.password,
-				:authentication => mail_credentials.authentication,
-				:enable_starttls_auto => mail_credentials.use_tls
-				},
-			:subject => @json.has_key?("subject") ? @json["subject"] : "",
-			:body => @json.has_key?("body") ? @json["body"] : "" )
-
-	rescue TimeoutError => e
-		puts "[Thread %s] FAIL. Connection timeout.".red % [Thread.current[:id]]
-
-	rescue IOError => e
-		puts "[Thread %s] FAIL. IOError.".red % [Thread.current[:id]]
-
-	rescue Exception => e
-		puts "[Thread %s] FAIL. %s".red % [Thread.current[:id], e.message]
-	end
-
-end
 
---
+++ mailer.rb
@@ -0,0 +1,63 @@
+class Mailer
+
+	attr_reader :user_name
+	attr_reader :password
+	attr_reader :port
+	attr_reader :address
+	attr_reader :from
+	attr_reader :authentication
+	attr_reader :enable_starttls_auto
+	attr_reader :use_tls
+
+	def initialize(credentials)
+		@credentials = credentials
+
+		@use_tls = get_credential("use_tls")
+		@user_name = get_credential("user_name")
+		@password = get_credential("password")
+		@port = get_credential("port")
+		@address = get_credential("address")
+		@from = get_credential("from")
+
+		@authentication = :plain
+		if @use_tls
+			@authentication = :cram_md5
+		end
+	end
+
+	def send(data)
+
+		begin
+			Pony.mail(:to => data.has_key?("to") ? data["to"] : raise('No mail recipient.'), 
+				:from => @from,
+				:via => :smtp, 
+				:via_options => {
+					:address => @address,
+					:port => @port,
+					:user_name => @user_name,
+					:password => @password,
+					:authentication => @authentication,
+					:enable_starttls_auto => @use_tls
+					},
+				:subject => data.has_key?("subject") ? data["subject"] : "",
+				:body => data.has_key?("body") ? data["body"] : "" )
+
+		rescue TimeoutError => e
+			puts "[Thread %s] FAIL. Connection timeout.".red % [Thread.current[:id]]
+
+		rescue IOError => e
+			puts "[Thread %s] FAIL. IOError.".red % [Thread.current[:id]]
+
+		rescue Exception => e
+			puts "[Thread %s] FAIL. %s".red % [Thread.current[:id], e.message]
+		end
+
+	end
+
+	private
+
+	def get_credential(key)
+		@credentials.has_key?(key) ? @credentials[key] : raise("Wrong user credentials. Missing '#{key}'.")
+	end
+
+end
