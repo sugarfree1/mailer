db85e019a5b8fd39e65ac3d70f871a1e0092ae7a
Dmitry Vershinin <dvershinin@rtcsoftware.ru>
2013-11-10 19:26:25 +0400

    Фикс передачи порта при запуске дочернего процесса через основной.

--- child.rb
+++ child.rb
@@ -7,10 +7,10 @@ require 'colorize'
 require './thread_pool'
 require './mailer'
 
-def run(pool_size, port="8000")
+def run(pool_size, port=8000)
 	$tp = ThreadPool.new(pool_size)
 	Mailer.instance # raise error if credentials are not full
-	Thin::Runner.new(["--port", port, "--address", "localhost", "--rackup", "rackup.ru", "start"]).run!
+	Thin::Runner.new(["--port", port.to_s, "--address", "localhost", "--rackup", "rackup.ru", "start"]).run!
 end
 
 post '/mail' do
