839a09775063bcb779cbfef0fc586f6d38623d9b
Dmitry Vershinin <dvershinin@rtcsoftware.ru>
2013-11-10 20:50:10 +0400

    Рефакторинг кода мастер-процесса.

--- master.rb
+++ master.rb
@@ -1,37 +1,45 @@
 require './loggers'
 
-def run_child(thread_pool_size, port)
-	spawn("ruby -r './child.rb' -e 'run(%s, %s)'" % [thread_pool_size.to_s, port.to_s])
-end
-
-def terminate_child(pid)
-	Process.kill("INT", pid)
-	Process.waitpid(pid)
-end
-
-def is_running?(pid)
-	begin
-		Process.waitpid(pid, Process::WNOHANG) == nil
-	rescue Errno::ECHILD => e
-		false
+class MasterProcessController
+	def initialize
+		@logger = MasterProcessLogger.new
 	end
-end
 
-def run(thread_pool_size, port)
-	master_logger = MasterProcessLogger.new
+	def run(thread_pool_size, port)
 
-	pid = run_child(thread_pool_size, port)
-	master_logger.started(pid)
+		pid = run_child(thread_pool_size, port)
+		@logger.started(pid)
 
-	while true do
-		trap("INT") { terminate_child(pid); exit() }
-		sleep(3)
-		if not is_running?(pid)
-			master_logger.terminated(pid)
-			pid = run_child(thread_pool_size, port)
-			master_logger.started(pid)
+		while true do
+			trap("INT") { terminate_child(pid); exit() }
+			sleep(3)
+			if not is_child_running?(pid)
+				@logger.terminated(pid)
+				pid = run_child(thread_pool_size, port)
+				@logger.started(pid)
+			end
 		end
 	end
-end
 
+	private
+
+	def run_child(thread_pool_size, port)
+		spawn("ruby -r './child.rb' -e 'run(%s, %s)'" % [thread_pool_size.to_s, port.to_s])
+	end
+
+	def terminate_child(pid)
+		Process.kill("INT", pid)
+		Process.waitpid(pid)
+		@logger.terminated(pid)
+	end
+
+	def is_child_running?(pid)
+		begin
+			Process.waitpid(pid, Process::WNOHANG) == nil
+		rescue Errno::ECHILD => e
+			false
+		end
+	end
+
+end
 
